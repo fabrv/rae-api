cf26de4e8b9d852fc24483657ea227b6b3ab4b4b
fabrv <frivera@nevicad.com>
2021-01-02 22:52:17 -0600

    basic rae api

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/pdd", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"word":       wordOfTheDay(),
+			"definition": getDefinition(wordOfTheDay()),
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET("/definicion/:word", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"word":       c.Param("word"),
+			"definition": getDefinition("/" + c.Param("word")),
+		})
+	})
+	r.Run()
 }
